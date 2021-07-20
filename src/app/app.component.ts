23043b245a200288a6b9234de9522e9ff4b441f1
Max <jmksm96@gmail.com>
2021-07-20 17:18:52 +0300

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,20 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    toggle = false
+    // arr = [1, 3, 5, 7, 9, 11]
+
+    obj = [
+        {title: 'post1', author: 'some guy', comments: [
+                {name: 'Max', text: 'text1'},
+                {name: 'Max', text: 'text1'},
+                {name: 'Max', text: 'text1'}
+            ]},
+        {title: 'post2', author: 'some guy2', comments: [
+                {name: 'Max2', text: 'text2'},
+                {name: 'Max2', text: 'text2'},
+                {name: 'Max2', text: 'text2'}
+            ]}
+    ]
 }
 
 
