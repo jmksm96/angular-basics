4bc20ed0ea188cda4513a1d276b80f06b8a6c877
Max <jmksm96@gmail.com>
2021-07-21 18:20:43 +0300

    Add event's

--- src/app/derectivies/style.directive.ts
+++ src/app/derectivies/style.directive.ts
@@ -1,8 +1,10 @@
-import {AfterViewInit, Directive, ElementRef, Input, Renderer2} from '@angular/core';
+import {AfterViewInit, Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
     selector: '[appStyle]'
 })
+
+
 export class StyleDirective implements AfterViewInit {
 
     @Input() color: string = 'red'
@@ -15,5 +17,18 @@ export class StyleDirective implements AfterViewInit {
         this.r.setStyle(this.el.nativeElement, 'color', this.color)
     }
 
+    @HostListener('click', ['$event']) onClick(event: Event) {
+        console.log(event)
+    }
+
+    @HostListener('mouseenter') onEnter() {
+        this.r.setStyle(this.el.nativeElement, 'color', this.color)
+    }
+
+    @HostListener('mouseleave') onLeave() {
+        this.r.setStyle(this.el.nativeElement, 'color', null)
+    }
 }
+
+
 
