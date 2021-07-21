b950e872e1454713edef3f18f77940aaa5994912
Max <jmksm96@gmail.com>
2021-07-21 17:36:54 +0300

    Directivies refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,20 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import {StyleDirective} from "./derectivies/derective";
+import {StyleDirective} from "./derectivies/style.directive";
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    StyleDirective,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        StyleDirective
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
