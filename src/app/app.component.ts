82fb9fce7cd6fd8a6eac49544fa88d4e2e9f3fb1
Max <jmksm96@gmail.com>
2021-07-23 16:08:17 +0300

    5.8 list filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import {Component} from '@angular/core'
 
+export interface Post {
+    title: string
+    text: string
+}
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    str: string = 'hello world'
+    search = ''.toLowerCase()
+    searchField = 'title'
+
+    posts: Post [] = [
+        {title: 'Angular', text: 'The best framework'},
+        {title: 'React', text: 'Bullsheet'},
+        {title: 'Vue', text: 'NoName'},
+    ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {NgModule} from '@angular/core'
 import {AppComponent} from './app.component'
 import {FormsModule} from '@angular/forms'
 import {ExMarksPipe} from './pipes/ex-marks.pipe';
+import {FilterPipe} from './pipes/filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExMarksPipe
+    ExMarksPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
