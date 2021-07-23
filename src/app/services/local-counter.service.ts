c0b51b022fbc950d63f2dbb157ce9bafa1493fc7
Max <jmksm96@gmail.com>
2021-07-23 17:47:38 +0300

    6.3 Local service

---
+++ src/app/services/local-counter.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class LocalCounterService {
+
+    counter = 0
+
+    increase() {
+        this.counter++
+    }
+
+    decrease() {
+        this.counter--
+    }
+
+    clear() {
+        this.counter = 0
+    }
+}
