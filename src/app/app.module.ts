34a3432d84e3b2e337f1c8d60824dc3190f254f8
Max <jmksm96@gmail.com>
2021-07-23 15:30:40 +0300

    5.7.1 Generate Pipes with cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
 import {FormsModule} from '@angular/forms'
-import {MultByPipe} from "../pipes/mult-by.pipe";
+import {ExMarksPipe} from './pipes/ex-marks.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MultByPipe
+    ExMarksPipe
   ],
   imports: [
     BrowserModule,
